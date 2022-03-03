fa2ec5782ad14747b1f8f57b57dce6a0e2d0a6f0
heegu0311 <heegu0311@gmail.com>
2022-03-04 00:38:15 +0900

    feat: Add Status component

---
+++ src/components/Status/index.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Status = styled.div`
+  height: 24px;
+  width: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 2px 7px;
+  font-size: 12px;
+  position: static;
+  background: #ffffff;
+  border: 1px solid #ffa000;
+  color: #ffa000;
+  box-sizing: border-box;
+  border-radius: 12px;
+`;
