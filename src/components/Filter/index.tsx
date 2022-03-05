87bae2359592b11a976c2a03b10757d154728620
heegu0311 <heegu0311@gmail.com>
2022-03-06 02:40:09 +0900

    fix: Update filter state logic & update css

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 import { FiRotateCw } from "react-icons/fi";
+import { useEffect, useState } from "react";
+
+interface Props {
+  selected: boolean;
+}
 
 const BaseFilter = styled.div`
   display: flex;
@@ -10,18 +15,31 @@ const BaseFilter = styled.div`
   border-radius: 4px;
   padding: 4px 12px;
   font-size: 12px;
+  letter-spacing: 0px;
   border: 1px solid #939fa5;
   &:hover {
     border: 1px solid #2196f3;
   }
 `;
 
-const SmallFilter = styled(BaseFilter)`
-  width: 76px;
+const SmallFilter = styled(BaseFilter)<Props>`
+  width: ${({ selected }) => (selected ? "91px" : "76px")};
+  background-color: ${({ selected }) => selected && "#1565C0"};
+  color: ${({ selected }) => selected && "#ffffff"};
+  * {
+    border-top: ${({ selected }) =>
+      selected ? "5px solid #ffffff" : "5px solid ##939fa5"} !important;
+  }
 `;
 
-const LargeFilter = styled(BaseFilter)`
+const LargeFilter = styled(BaseFilter)<Props>`
   width: 98px;
+  background-color: ${({ selected }) => selected && "#1565C0"};
+  color: ${({ selected }) => selected && "#ffffff"};
+  * {
+    border-top: ${({ selected }) =>
+      selected ? "5px solid #ffffff" : "5px solid ##939fa5"} !important;
+  }
 `;
 
 const TriangleDown = styled.div`
@@ -30,20 +48,32 @@ const TriangleDown = styled.div`
   border-top: 5px solid #939fa5;
 `;
 
-export function SmallFilterBtn({ children }: { children: string }) {
+export function SmallFilterBtn({
+  children,
+  selected,
+}: {
+  children: string;
+  selected: boolean;
+}) {
   // TODO : 선택시 색상 필터 배경섹 변경
   return (
-    <SmallFilter>
+    <SmallFilter selected={selected}>
       {children}
       <TriangleDown />
     </SmallFilter>
   );
 }
 
-export function LargeFilterBtn({ children }: { children: string }) {
+export function LargeFilterBtn({
+  children,
+  selected,
+}: {
+  children: string;
+  selected: boolean;
+}) {
   // TODO : 선택시 색상 필터 배경섹 변경
   return (
-    <LargeFilter>
+    <LargeFilter selected={selected}>
       {children}
       <TriangleDown />
     </LargeFilter>
@@ -53,16 +83,18 @@ export function LargeFilterBtn({ children }: { children: string }) {
 const CheckBoxes = styled.div`
   height: auto;
   width: 130px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
   align-items: start;
   border: 1px solid #939fa5;
   border-radius: 4px;
   padding: 17px 12px;
-  gap: 9px;
   z-index: 10;
   background: var(--color-white);
+  form {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 9px;
+  }
   div {
     display: flex;
     justify-content: start;
@@ -88,42 +120,73 @@ const CheckBoxes = styled.div`
 
 export function CheckBoxContainer({
   filterName,
-  list,
   filter,
+  list,
   setFilter,
 }: {
   filterName: string;
-  list: string[];
   filter: string[];
+  list: string[] | null;
   setFilter: (active: string[]) => void;
 }): JSX.Element {
-  const addFilter = (
-    e: React.MouseEvent<HTMLInputElement, MouseEvent>
-  ): void => {
-    const { value, checked } = e.target as HTMLInputElement;
-    if (checked) filter.push(value);
-    else {
-      filter.splice(Number(filter.findIndex((el: string) => el === value)), 1);
+  const [checkedItems, setCheckedItems] = useState<any>(new Set());
+  const [bChecked, setChecked] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (filter.length) filter.forEach((el) => checkedItems.add(el));
+  }, [checkedItems, filter]);
+
+  const checkedItemHandler = (el: string, isChecked: boolean) => {
+    if (isChecked) {
+      checkedItems.add(el);
+      setCheckedItems(checkedItems);
+    } else if (!isChecked && checkedItems.has(el)) {
+      checkedItems.delete(el);
+      setCheckedItems(checkedItems);
     }
-    setFilter(Array.from(new Set(filter)));
+    const sorted: string[] = Array.from(checkedItems);
+    sorted.sort();
+    setFilter(sorted);
+  };
+
+  const checkHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const { value, checked } = e.target as HTMLInputElement;
+    setChecked(!bChecked);
+    checkedItemHandler(value, checked);
+  };
+
+  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>): void => {
+    e.preventDefault();
   };
 
   return (
     <CheckBoxes>
-      {list.map(
-        (el: string, idx: number): JSX.Element => (
-          <div key={el}>
-            <input
-              type="checkbox"
-              id={idx.toString()}
-              name={filterName}
-              value={el}
-              onClick={addFilter}
-            />
-            <label htmlFor={idx.toString()}>{el}</label>
-          </div>
-        )
-      )}
+      <form onSubmit={handleSubmit}>
+        {list?.map(
+          (el: string, idx: number): JSX.Element => (
+            <div key={el}>
+              {filter.includes(el) ? (
+                <input
+                  type="checkbox"
+                  id={idx.toString()}
+                  name={filterName}
+                  value={el ?? ""}
+                  onChange={checkHandler}
+                />
+              ) : (
+                <input
+                  type="checkbox"
+                  id={idx.toString()}
+                  name={filterName}
+                  value={el ?? ""}
+                  onChange={checkHandler}
+                />
+              )}
+              <label htmlFor={idx.toString()}>{el}</label>
+            </div>
+          )
+        )}
+      </form>
     </CheckBoxes>
   );
 }
@@ -139,9 +202,20 @@ const FlexContainer = styled.div`
   }
 `;
 
-export function ResetBtn(): JSX.Element {
+export function ResetBtn({
+  setMethod,
+  setMaterial,
+}: {
+  setMethod: (active: string[]) => void;
+  setMaterial: (active: string[]) => void;
+}): JSX.Element {
   return (
-    <FlexContainer>
+    <FlexContainer
+      onClick={() => {
+        setMethod([]);
+        setMaterial([]);
+      }}
+    >
       <FiRotateCw size={20} />
       <span>필터링 리셋</span>
     </FlexContainer>
