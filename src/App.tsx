715d7f3fe067d06a67c3cc0a459913f6ac50cef3
heegu0311 <heegu0311@gmail.com>
2022-03-06 02:41:17 +0900

    feat: Add Empty component & filtering logic of request list

--- src/App.tsx
+++ src/App.tsx
@@ -40,6 +40,11 @@ const FlexColumn = styled(Flex)`
   flex-direction: column;
 `;
 
+const FlexContainer = styled(FlexColumn)`
+  min-height: 880px;
+  min-width: 1130;
+`;
+
 const FlexColumnFilter = styled(FlexColumn)`
   max-width: 98px;
   height: auto;
@@ -103,13 +108,30 @@ const Filter = styled(FlexCenter)`
 
 const Grid = styled.div`
   display: grid;
-  grid-template-columns: auto auto auto;
+  grid-template-columns: 366px 366px 366px;
   gap: 16px;
   ${media.greaterThan("medium")`
     min-width: 1130px;
   `}
   ${media.lessThan("medium")`
     grid-template-columns: auto;
+  `}
+`;
+
+const Empty = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+  width: 1130px;
+  border-radius: 4px;
+  border: 1px solid #c2c2c2;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 20px;
+  color: #939fa5;
+  ${media.lessThan("medium")`
+    width: auto;
   `}
 `;
 
@@ -129,13 +151,10 @@ interface RequestData {
 function App() {
   const [toggleClicked, setToggleClicked] = useState<boolean>(false);
   const [reqList, setReqList] = useState<RequestData[]>([]);
+  const [filteredList, setFilteredList] = useState<RequestData[]>([]);
   const [dropdown, setDropdown] = useState<string | null>(null);
   const [method, setMethod] = useState<string[]>([]);
   const [material, setMaterial] = useState<string[]>([]);
-
-  function handleToggleOnOff(): void {
-    setToggleClicked(!toggleClicked);
-  }
 
   const materialList: string[] = [
     "알루미늄",
@@ -144,17 +163,18 @@ function App() {
     "합금강",
     "강철",
   ];
-  const process: string[] = ["밀링", "선반"];
+  const methodList: string[] = ["밀링", "선반"];
+
+  async function getRequests(): Promise<void> {
+    try {
+      const { data }: { data: RequestData[] } = await axios.get(
+        "http://localhost:3000/requests"
+      );
+      setReqList(data);
+    } catch (error) {}
+  }
 
   useEffect((): void => {
-    async function getRequests(): Promise<void> {
-      try {
-        const { data }: { data: RequestData[] } = await axios.get(
-          "http://localhost:3000/requests"
-        );
-        setReqList(data);
-      } catch (error) {}
-    }
     getRequests();
   }, []);
 
@@ -166,11 +186,74 @@ function App() {
     if (e.type === "mouseenter") setDropdown(textContent);
   };
 
+  const isSubsetOf = (base: string[], sample: string[]): boolean => {
+    base.sort();
+    sample.sort();
+
+    const findItemInSortedArr = (
+      item: string,
+      arr: string[],
+      from: number
+    ): number => {
+      for (let i = from; i < arr.length; i++) {
+        if (item === arr[i]) return i;
+        else if (item < arr[i]) return -1;
+      }
+      return -1;
+    };
+
+    let baseIdx = 0;
+    for (let i = 0; i < sample.length; i++) {
+      baseIdx = findItemInSortedArr(sample[i], base, baseIdx);
+      if (baseIdx === -1) return false;
+    }
+    return true;
+  };
+
+  function handleToggleOnOff(): void {
+    setToggleClicked(!toggleClicked);
+  }
+
+  useEffect(() => {
+    const requests = reqList.filter((req) => {
+      if (method.length && material.length) {
+        // 교집합
+        const reqConditions = req.method.concat(req.material);
+        const conditions = method.concat(material);
+        if (isSubsetOf(reqConditions, conditions))
+          if (toggleClicked) {
+            return req.status === "상담중" && req;
+          } else return req;
+      } else if (method.length && !material.length) {
+        // 차집합 (재료)
+        if (isSubsetOf(req.method, method)) {
+          if (toggleClicked) {
+            return req.status === "상담중" && req;
+          } else return req;
+        }
+      } else if (!method.length && material.length) {
+        // 차집합 (가공방식)
+        if (isSubsetOf(req.material, material)) {
+          if (toggleClicked) {
+            return req.status === "상담중" && req;
+          } else return req;
+        }
+      } else {
+        // 필터 안됨
+        if (toggleClicked) {
+          return req.status === "상담중" && req;
+        } else return req;
+      }
+      return null;
+    });
+    setFilteredList(requests);
+  }, [material, method, reqList, toggleClicked]);
+
   return (
     <>
       <Header />
       <Page>
-        <FlexColumn>
+        <FlexContainer>
           <Title>들어온 요청</Title>
           <Desc>파트너에게 딱 맞는 요청서를 찾아보세요.</Desc>{" "}
           <FlexRows>
@@ -179,12 +262,14 @@ function App() {
                 onMouseEnter={handleDropdown}
                 onMouseLeave={handleDropdown}
               >
-                <LargeFilterBtn>가공방식</LargeFilterBtn>
+                <LargeFilterBtn selected={Boolean(method.length)}>
+                  가공방식
+                </LargeFilterBtn>
                 {dropdown === "가공방식" && (
                   <CheckBoxContainer
-                    filterName={dropdown}
-                    list={process}
+                    filterName={"dropdown"}
                     filter={method}
+                    list={methodList}
                     setFilter={setMethod}
                   />
                 )}
@@ -193,18 +278,20 @@ function App() {
                 onMouseEnter={handleDropdown}
                 onMouseLeave={handleDropdown}
               >
-                <SmallFilterBtn>재료</SmallFilterBtn>
-                {dropdown === "재료" && (
+                <SmallFilterBtn selected={Boolean(material.length)}>
+                  {material.length ? `재료(${material.length})` : "재료"}
+                </SmallFilterBtn>
+                {dropdown?.includes("재료") && (
                   <CheckBoxContainer
                     filterName={dropdown}
-                    list={materialList}
                     filter={material}
+                    list={materialList}
                     setFilter={setMaterial}
                   />
                 )}
               </FlexColumnFilter>
               <Filter>
-                <ResetBtn />
+                <ResetBtn setMethod={setMethod} setMaterial={setMaterial} />
               </Filter>
             </FlexRow>
             <FlexRow>
@@ -215,12 +302,16 @@ function App() {
               <div>상담중인 요청만 보기</div>
             </FlexRow>
           </FlexRows>
-          <Grid>
-            {reqList.map((el) => {
-              return <Request key={el.id} data={el} />;
-            })}
-          </Grid>
-        </FlexColumn>
+          {filteredList.length ? (
+            <Grid>
+              {filteredList.map((req) => (
+                <Request key={req.id} data={req} />
+              ))}
+            </Grid>
+          ) : (
+            <Empty>조건에 맞는 견적 요청이 없습니다.</Empty>
+          )}
+        </FlexContainer>
       </Page>
     </>
   );
